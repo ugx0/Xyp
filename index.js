e124d29461f8d6fb6eb326560b2ccd55986481b2
YuuJi <145370362+ugx0@users.noreply.github.com>
2025-03-10 20:41:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const wait = require('wait')
-require('dotenv').config()
-require('module-alias/register')
-const path = require('path')
-const Bitzxier = require(`./structures/Bitzxier.js`)
-const client = new Bitzxier()
-this.config = require(`${process.cwd()}/config.json`);
-(async () => {
-    await client.initializeMongoose()
-    await client.initializedata()
-    await wait(3000);
-    (await client.loadEvents()) - (await client.loadlogs())
-    await client.loadMain()
-    await client.login(this.config.TOKEN)
+const wait = require('wait');
+require('dotenv').config();
+require('module-alias/register');
+const path = require('path');
+const Bitzxier = require(`./structures/Bitzxier.js`);
+const client = new Bitzxier();
 
-})()
+// Use environment variables instead of config.json
+(async () => {
+    await client.initializeMongoose();
+    await client.initializedata();
+    await wait(3000);
+    (await client.loadEvents()) - (await client.loadlogs());
+    await client.loadMain();
+    
+    // Use process.env.TOKEN instead of this.config.TOKEN
+    await client.login(process.env.TOKEN);
+})();
